2be73dcc7fed60c6a3b453d269da23e330179804
Murilo Ribeiro <muriloribeiro11@gmail.com>
2024-11-28 23:19:49 -0300

    Entidades

---
+++ Entidades/Fornecedor.cs
@@ -0,0 +1,16 @@
+﻿namespace VistoriaZN.Entidades
+{
+    public class Fornecedor
+    {
+        public Fornecedor() { }
+        public Fornecedor(int Id, string Nome, string Telefone)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Telefone = Telefone;
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Telefone { get; set; }
+    }
+}
---
+++ Entidades/Vistoria.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace VistoriaZN.Entidades
+{
+    public class Vistoria
+    {
+        public Vistoria() {}
+        public Vistoria(int Id, string Nome, Fornecedor? Fornecedor, int? FornecedorId, int Valor, string Placa, string Modelo, DateTime DtRealizado, DateTime? DtPago)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Fornecedor = Fornecedor;
+            this.FornecedorId = FornecedorId;
+            this.Valor = Valor;
+            this.Placa = Placa;
+            this.Modelo = Modelo;
+            this.DtRealizado = DtRealizado ;
+            this.DtPago = DtPago;
+        }
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public Fornecedor? Fornecedor { get; set; }
+        public int? FornecedorId { get; set; }
+        public int Valor { get; set; }
+        public string Placa { get; set; }
+        public string Modelo { get; set; }
+        public DateTime DtRealizado { get; set; }
+        public DateTime? DtPago { get; set; }
+    }
+}
